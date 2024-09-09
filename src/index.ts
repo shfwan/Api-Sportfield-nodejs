6fcb0dc8300a300ac415812f7ab5f5480bd87f28
shfwan <shafwan367@gmail.com>
2024-09-10 06:01:32 +0900

    first 1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { serve } from '@hono/node-server'
-import app from './app/app'
+import { Hono } from 'hono'
 
-const port = 2722
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+const port = 3000
 console.log(`Server is running on port ${port}`)
 
 serve({
